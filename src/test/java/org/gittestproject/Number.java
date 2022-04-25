6e5e1ad4794a8aac001587e79cddaed069b8be00
keerthisasikumar20 <keerthisasikumar02@gmail.com>
2022-04-25 23:47:15 +0530

    Added number testproject

---
+++ src/test/java/org/gittestproject/Number.java
@@ -0,0 +1,10 @@
+package org.gittestproject;
+
+public class Number {
+public static void main(String[] args) {
+	System.out.println("hello member");
+	int a=1; int b=2;
+	int c=a+b;
+	System.out.println(c);
+}
+}
